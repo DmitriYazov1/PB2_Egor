cd25624b1aa8d51765b1b6cd76ad9ffccd68804c
Егор <flaweiwe24@mail.ru>
2025-11-03 17:20:06 +0300

    feat: Практика 7

---
+++ course-2/practices/c2-p7/c2-p7/Program.cs
@@ -0,0 +1,66 @@
+﻿
+
+
+class Car
+{
+
+   
+    public string Brand { get; }
+    public int Speed { get; private set; }
+
+    public Car(string name, int speed)
+    {
+         Speed = speed;
+         Brand = name;
+    }
+    public void Accel()
+        {
+            Speed += 10;
+        }
+}
+
+
+class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Pages { get; set; }
+    public Book(string title, string author, int pages)
+    {
+         Title = title;
+         Author = author;
+         Pages = pages;
+    }
+
+    public void Read(int pages_read)
+    {
+        if (pages_read > Pages || pages_read < 0)
+        {
+            Console.WriteLine($"Error");
+        }
+        else
+        {
+            Console.WriteLine($"Вы прочитали {pages_read} страниц из {Pages}");
+        }
+    }
+}
+
+class Library
+{
+    private List<Book> books = new List<Book>();
+
+    public void AddBook(Book book)
+    {
+        books.Add(book);
+    }
+
+    public void PrintAllBooks()
+    {
+        foreach (var book in books)
+        {
+            Console.WriteLine(book.Title);
+        }
+    }
+}
+
+
