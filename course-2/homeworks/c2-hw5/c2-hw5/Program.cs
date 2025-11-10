9021c118a29f0e83652961edf8517412d0c596a5
Егор <flaweiwe24@mail.ru>
2025-11-10 15:56:54 +0300

    feat: Добавлена домашняя работа 8

---
+++ course-2/homeworks/c2-hw5/c2-hw5/Program.cs
@@ -0,0 +1,71 @@
+﻿class Movie
+{
+    public string Title { get; set; }
+    public string Genre { get; set; }
+    public double Rating { get; set; }
+
+    public Movie() : this("Без названия", "Неизвестен", 0)
+    {
+    }
+
+    public Movie(string title) : this(title, "Неизвестен", 0)
+    {
+    }
+
+    public Movie(string title, string genre, double rating)
+    {
+        this.Title = title;
+        this.Genre = genre;
+        this.Rating = rating;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Название: {Title}, жанр: {Genre}, рейтинг: {Rating}");
+    }
+}
+
+class Cat
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Cat(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"{Name}, возраст {Age}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var m1 = new Movie();
+        var m2 = new Movie("Матрица");
+        var m3 = new Movie("Начало", "Фантастика", 9);
+
+        m1.PrintInfo();
+        m2.PrintInfo();
+        m3.PrintInfo();
+
+        var cat1 = new Cat("Барсик", 2);
+        var cat2 = cat1;
+        cat2.Age = 5;
+
+        cat1.PrintInfo();
+        cat2.PrintInfo();
+
+        var cat3 = new Cat(cat1.Name, cat1.Age);
+        cat3.Age = 10;
+
+        cat1.PrintInfo();
+        cat2.PrintInfo();
+        cat3.PrintInfo();
+    }
+}
---
+++ course-2/practices/c2-p8/c2-p8/Program.cs
@@ -0,0 +1,162 @@
+﻿class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int PageCount { get; set; }
+
+    public Book()
+    {
+        Title = "Неизвестно";
+        Author = "Неизвестно";
+        PageCount = 0;
+    }
+
+    public Book(string title)
+    {
+        Title = title;
+        Author = "Неизвестно";
+        PageCount = 0;
+    }
+
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+        PageCount = 0;
+    }
+
+    public Book(string title, string author, int pageCount)
+    {
+        Title = title;
+        Author = author;
+        PageCount = pageCount;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Книга: {Title}, Автор: {Author}, Страниц: {PageCount}");
+    }
+}
+
+class Car
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Speed { get; set; }
+
+    public Car() : this("Неизвестно", "Неизвестно", 0)
+    {
+    }
+
+    public Car(string brand) : this(brand, "Неизвестно", 0)
+    {
+    }
+
+    public Car(string brand, string model, int speed)
+    {
+        this.Brand = brand;
+        this.Model = model;
+        this.Speed = speed;
+    }
+}
+
+class Player
+{
+    public string Name;
+    public int Level;
+    public void PrintInfo()
+    {
+        Console.WriteLine($"{Name}, {Level}");
+    }
+    
+}
+
+
+
+class User
+{
+    private string password;
+    public string Name;
+
+    public string Password
+    {
+        get { return password; }
+
+        set
+        {
+            if (value != null)
+            {
+                password = value;
+                Console.WriteLine("Пароль обновлен");
+            }
+        }
+    }
+    
+    public bool CheckPassword(string input)
+    {
+        return password == input;
+    }
+}
+
+class Point
+{
+    public int X;
+    public int Y;
+
+    public void Move(int dx, int dy)
+    {
+        X += dx;
+        Y += dy;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //1 
+        var b1 = new Book();
+        var b2 = new Book("1984");
+        var b3 = new Book("Гроза", "Островский");
+        var b4 = new Book("Гарри Поттер", "Дж. Роулинг", 500);
+
+        b1.PrintInfo();
+        b2.PrintInfo();
+        b3.PrintInfo();
+        b4.PrintInfo();
+
+        //3
+
+        Player p1 = new Player();
+        p1.Name = "Алекс";
+
+        Player p2 = new Player();
+        p2.Name = "Алекс";
+
+        Console.WriteLine($"{p1 == p2}");
+
+        Console.WriteLine($"{Equals(p1, p2)}");
+
+        //4
+        var user = new User();
+        user.Name = "admin";
+        user.Password = "12345";
+        Console.WriteLine(user.CheckPassword("12345"));
+        user.Password = "";
+
+        //5
+        Point p1_point = new Point();
+        p1_point.X = 10;
+        p1_point.Y = 20;
+
+        Point p2_point = p1_point;
+
+        Console.WriteLine($"До изменения: p1_point = ({p1_point.X}, {p1_point.Y}), p2_point = ({p2_point.X}, {p2_point.Y})");
+
+        p2_point.Move(5, 5);
+
+        Console.WriteLine($"После изменения p2_point: p1_point = ({p1_point.X}, {p1_point.Y}), p2_point = ({p2_point.X}, {p2_point.Y})");
+
+
+    }
+}
