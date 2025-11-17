97ec426003bdef85061d32e0ecefb8e25269aa7e
Егор <flaweiwe24@mail.ru>
2025-11-17 16:05:54 +0300

    feat:Добавлено 6 домашнее задание

---
+++ course-2/homeworks/c2-hw6/c2-hw6/Program.cs
@@ -0,0 +1,97 @@
+﻿class Transport
+{
+    public string Model { get; set; }
+    protected int Speed = 0;
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Модель: {Model}, скорость: {Speed} км/ч");
+
+    }
+
+
+    public virtual void Move()
+    {
+        Console.WriteLine("Транспорт движется");
+    }
+}
+
+class Car : Transport
+{
+    public void Accelerate(int value)
+    {
+        if (value + Speed <= 200 && 0 <= value + Speed)
+        {
+            Speed += value;
+        }
+    }
+    override public void  Move()
+    {
+        Console.WriteLine("Машина едет по дороге");
+    }
+}
+
+class Bicycle: Transport
+{
+    public void Pedal()
+    {
+        Speed += 5;
+    }
+
+    override public void Move()
+    {
+        Console.WriteLine("Велосипед крутит педали");
+    }
+}
+
+
+
+
+
+class Account
+{
+    private decimal balance;
+    public string Owner { get; set; }
+
+
+    public decimal Balance { get { return balance; } }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount >= 0)
+        {
+            balance += amount;
+        }
+    }
+    public void Withdraw(decimal amount)
+    {
+        if (balance >= amount)
+        {
+            balance -= amount;
+        }
+    }
+
+    public void ShowBalance()
+    {
+        Console.WriteLine($"Баланс: {Balance} руб.");
+    }
+
+}
+
+
+class SavingsAccount : Account
+{
+    private decimal interestRate;
+
+    public SavingsAccount(string owner, decimal rate)
+    {
+        Owner= owner;
+        interestRate = rate;
+
+    }
+
+    public void AddInterest()
+    {
+        Deposit(Balance * (interestRate / 100));
+    }
+}
---
+++ course-2/practices/c2-p10/c2-p10/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Net.Cache;
+
+class Person
+{
+    private int age;
+    private string name;
+
+    public int Age
+    {
+        get { return age; }
+
+        set
+        {
+            if (value < 0)
+            {
+                age = value; 
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+        
+    }
+
+    public string Name
+    {
+        get { return name; }
+
+        set
+        {
+            if (value !="")
+            {
+                name = value;
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+    }
+
+}
+
+class BankAccount
+{
+    private decimal balance;
+
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount > 0 && balance>=amount)
+        {
+            balance -= amount;
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+    }
+
+    public void ShowBalance()
+    {
+        Console.WriteLine(balance);
+    }
+
+}
+
+class Thermometer
+{
+    private decimal temperatureCelsius;
+    public decimal TemperatureCelsius {
+        get { return temperatureCelsius; }
+        set { if (value >= -273)
+            {
+                temperatureCelsius = value;
+            } 
+        }
+    } 
+
+    public decimal TemperatureFahrenheit
+    {
+        get { return (temperatureCelsius * 9 / 5) + 32; }
+
+    }
+}
+
+class Animal
+{
+    protected int energy = 100;
+
+    public void Eat()
+    {
+        energy += 10;
+    }
+}
+
+class Dog : Animal
+{
+    public void Run()
+    {
+        energy -=20;
+    }
+}
