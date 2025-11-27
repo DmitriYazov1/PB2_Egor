f0ab4d24faf02f2a122f0b7d31677ebad5f198e8
Егор <flaweiwe24@mail.ru>
2025-11-27 17:16:36 +0300

    feat:Добавлена практика 13

---
+++ course-2/practices/c2-p12/c2-p12/Program.cs
@@ -0,0 +1,200 @@
+﻿//1
+
+abstract class Shape
+{
+   public abstract string Name{ get;  }
+   public abstract double GetArea();
+
+    public void Print()
+    {
+        Console.WriteLine($"{Name}: площадь = {GetArea()}");
+    }
+}
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+    public override string Name => "Круг";
+
+    public override double GetArea() => Math.PI * Radius * Radius;
+    
+
+}
+
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public override string Name => "Прямоугольник";
+
+    public override double GetArea() => Width * Height;
+
+}
+
+//2
+abstract class Animal
+{
+    public string Name;
+    public Animal(string name)
+    {
+        Name=name;
+        Console.WriteLine($"Создано животное: {Name}");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine($"{Name} ест.");
+    }
+
+    public abstract void MakeSound();
+}
+class Dog: Animal
+{
+
+    public Dog(string name) : base(name) { }
+    
+    
+
+    public override void MakeSound()
+    {
+        Console.WriteLine($"{Name}: Гав-гав!");
+    }
+}
+
+
+class Cat : Animal
+{
+
+    public Cat(string name) : base(name) { }
+
+
+
+    public override void MakeSound()
+    {
+        Console.WriteLine($"{Name}: Мяу!");
+    }
+}
+//3
+
+abstract class Transport
+{
+    protected void Start()
+    {
+        Console.WriteLine("Start");
+    }
+
+    protected void Stop()
+    {
+        Console.WriteLine("Stop");
+    }
+
+    protected abstract void MoveCore();
+    public void Move()
+    {
+        Start(); 
+        MoveCore(); 
+        Stop();
+    }
+}
+class Car : Transport
+{
+    protected override void MoveCore()
+    {
+        Console.WriteLine("Машина едет по дороге");
+    }
+}
+
+class Boat : Transport
+{
+    protected override void MoveCore()
+    {
+        Console.WriteLine("Лодка плывёт по воде");
+    }
+}
+
+// 
+public abstract class DocumentExporter
+{
+    public abstract string FormatName { get; }
+    public abstract void Export(string content);
+
+    public void ShowInfo(string content)
+    {
+        Console.WriteLine($"Экспорт в формат {FormatName}: {content}");
+    }
+}
+
+public class TxtExporter : DocumentExporter
+{
+    public override string FormatName => "TXT";
+
+    public override void Export(string content)
+    {
+        Console.WriteLine("Сохраняем текстовый файл");
+    }
+}
+
+public class PdfExporter : DocumentExporter
+{
+    public override string FormatName => "PDF";
+
+    public override void Export(string content)
+    {
+        Console.WriteLine("Создаём PDF-документ");
+    }
+}
+
+class Programm
+{
+    public void Main()
+    {
+        //1
+        Shape[] shapes =
+        {
+            new Circle{Radius=5},
+            new Rectangle{Width=5, Height=8},
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            shape.Print();
+        }
+        //2
+        Animal[] animals =
+       {
+            new Dog("Рекс"),
+            new Cat("Мурка")
+        };
+
+        foreach (var animal in animals)
+        {
+            animal.Eat();
+            animal.MakeSound();
+        }
+        //3
+        Transport[] transports =
+      {
+            new Car(),
+            new Boat()
+        };
+
+        foreach (var transport in transports)
+        {
+            transport.Move();
+        }
+
+        //4
+        DocumentExporter[] exporters =
+        {
+            new TxtExporter(),
+            new PdfExporter()
+        };
+
+        foreach (var exporter in exporters)
+        {
+            exporter.ShowInfo("Hello world!");
+            exporter.Export("Hello world!");
+        }
+    }
+}
---
+++ course-2/practices/c2-p13/c2-p13/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+//1
+interface IPlayble
+{
+    void Play();
+}
+
+class Guitar : IPlayble
+{
+    public void Play()
+    {
+        Console.WriteLine("Гитара играет аккорды");
+    }
+}
+
+class Piano : IPlayble
+{
+    public void Play()
+    {
+        Console.WriteLine("Пианино играет мелодию");
+    }
+}
+
+class Drum : IPlayble
+{
+    public void Play()
+    {
+        Console.WriteLine("Барабан отбивает ритм");
+    }
+}
+
+//2
+interface IReadble
+{
+    void Read(string filename);
+}
+
+interface IWriteble
+{
+    void Write(string filename, string content);
+}
+
+interface ISaveble
+{
+    void Save();
+}
+
+class Textdocument : IReadble, IWriteble, ISaveble
+{
+    private string content;
+    public void Read(string filename)
+    {
+        Console.WriteLine($"Чтение из файла {filename}");
+        content = "текст из файла";
+    }
+
+    public void Write(string filename, string content)
+    {
+        Console.WriteLine($"Запись в файл {filename}: {content}");
+        this.content = content;
+    }
+
+    public void Save()
+    {
+        Console.WriteLine($"Файл сохранён. Содержимое: {content}");
+    }
+}
+
+//3
+interface IDocumentExporter
+{
+    string FormatName { get; }
+    void Export(string content);
+
+    void ShowInfo(string content)
+    {
+        Console.WriteLine($"Экспорт в формат {FormatName}: {content}");
+    }
+}
+
+class TxtExporter : IDocumentExporter
+{
+    public string FormatName => "TXT";
+
+    public void Export(string content)
+    {
+        Console.WriteLine("Сохраняем текстовый файл...");
+    }
+}
+
+class PdfExporter : IDocumentExporter
+{
+    public string FormatName => "PDF";
+
+    public void Export(string content)
+    {
+        Console.WriteLine("Создаём PDF-документ...");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //1
+        IPlayble[] instruments =
+        {
+            new Guitar(),
+            new Piano(),
+            new Drum()
+        };
+        foreach (var instrument in instruments)
+        {
+            instrument.Play();
+        }
+
+        Console.WriteLine();
+
+        //2
+        Textdocument textdocument = new Textdocument();
+        textdocument.Read("data.txt");
+        textdocument.Write("data.txt", "Привет, мир!");
+        textdocument.Save();
+
+        Console.WriteLine();
+
+        //3
+        IDocumentExporter[] exporters =
+        {
+            new TxtExporter(),
+            new PdfExporter()
+        };
+
+        foreach (var exporter in exporters)
+        {
+            exporter.ShowInfo("Hello World!");
+            exporter.Export("Hello World!");
+        }
+    }
+}
